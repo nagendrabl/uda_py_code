145a1ae49a232db74b0aae5d06e8adb3966221c1
Nagendra Bangalore <nagendradanii@gmail.com>
2015-08-26 22:16:38 +0530

    Second Checkin of source

--- takebreak.py
+++ takebreak.py
@@ -5,9 +5,10 @@ loop=0
 total_breaks = 3;
 while loop != total_breaks:
   print ("printing for the time and it started at " + time.ctime())
-  loop = loop+1
   time.sleep(10)
-
+  loop = loop+1
+  webbrowser.open("www.google.com")  
+  
 print ("Iam out of the loop")
 
   
